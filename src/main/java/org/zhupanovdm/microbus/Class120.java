d6af97ef0598b151fef3138936a8a4266e64cdd3
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-04-08 22:15:45 +0300

    Grooming

--- src/main/java/org/zhupanovdm/microbus/Class120.java
+++
@@ -1,7 +0,0 @@
-package org.zhupanovdm.microbus;
-
-import org.zhupanovdm.microbus.core.annotations.ActiveObject;
-
-@ActiveObject
-public class Class120 {
-}
--- src/main/java/org/zhupanovdm/microbus/SampleApp.java
+++
@@ -1,9 +0,0 @@
-package org.zhupanovdm.microbus;
-
-import org.zhupanovdm.microbus.core.App;
-
-public class SampleApp {
-    public static void main(String[] args) {
-        App.run(SampleApp.class, args);
-    }
-}
